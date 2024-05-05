b84e5b76ad8d9653691ab4c441333cce0a6a1585
gabriel.freitas <gabriel.freitas3110@gmail.com>
2024-05-04 21:16:02 -0300

    #Error Handler

--- str-consumer/src/main/java/com/example/strconsumer/listeners/StrConsumerListeners.java
+++ str-consumer/src/main/java/com/example/strconsumer/listeners/StrConsumerListeners.java
@@ -1,6 +1,7 @@
 package com.example.strconsumer.listeners;
 
 import com.example.strconsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListeners {
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("\nCREATE ::: Receive message: \n{}.", message);
+        throw new IllegalAccessException("EXCEPTION...");
     }
 
     @StrConsumerCustomListener(groupId = "group-1")
