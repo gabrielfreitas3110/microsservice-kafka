45224511ba955c877ca03d8ec01c24a40c85e6d6
gabriel.freitas <gabriel.freitas3110@gmail.com>
2024-05-09 18:47:32 -0300

    #Dockerfile config

--- json-consumer/src/main/java/com/example/jsonconsumer/listener/JsonListener.java
+++ json-consumer/src/main/java/com/example/jsonconsumer/listener/JsonListener.java
@@ -4,6 +4,7 @@ package com.example.jsonconsumer.listener;
 import com.example.jsonconsumer.model.Payment;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -15,7 +16,7 @@ import static java.lang.Thread.sleep;
 public class JsonListener {
 
     @SneakyThrows
-    @KafkaListener(topics = "${topic.name}", groupId = "create-group", containerFactory = "jsonContainerFactory")
+    @KafkaListener(topics = "{$topic.name}", groupId = "create-group", containerFactory = "jsonContainerFactory")
     public void antiFraude(@Payload Payment payment) {
         log.info("Recebi o pagamento {}", payment);
         sleep(2000);
@@ -26,14 +27,14 @@ public class JsonListener {
     }
 
     @SneakyThrows
-    @KafkaListener(topics = "${topic.name}", groupId = "pdf-group", containerFactory = "jsonContainerFactory")
+    @KafkaListener(topics = "{$topic.name}", groupId = "pdf-group", containerFactory = "jsonContainerFactory")
     public void pdfGenerator(@Payload Payment payment) {
         log.info("Gerando pdf do produto de id {}...", payment.getId());
         sleep(2000);
     }
 
     @SneakyThrows
-    @KafkaListener(topics = "${topic.name}", groupId = "email-group", containerFactory = "jsonContainerFactory")
+    @KafkaListener(topics = "{$topic.name}", groupId = "email-group", containerFactory = "jsonContainerFactory")
     public void sendEmail() {
         log.info("Enviando email de confirmação...");
     }
